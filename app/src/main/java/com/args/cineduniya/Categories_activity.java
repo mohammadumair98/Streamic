62ee98495c9c02a6d935bbcb7d0b5e70435bb76f
adityakamath16 <61751718+adityakamath16@users.noreply.github.com>
2020-04-07 11:43:47 +0530

    small bugs fixed...

--- app/src/main/java/com/args/cineduniya/Categories_activity.java
+++ app/src/main/java/com/args/cineduniya/Categories_activity.java
@@ -5,19 +5,60 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class Categories_activity extends AppCompatActivity {
 
+    ImageView kannada,kodava;
     TextView goback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories_activity);
 
+
+        kannada = findViewById(R.id.kannada_category);
+        kodava = findViewById(R.id.kodava_category);
         //to go back home
         goback=findViewById(R.id.gohomecategory);
 
+        Picasso.get()
+                .load("https://upload.wikimedia.org/wikipedia/en/5/53/Parasite_%282019_film%29.png")
+                .fit()
+                .centerInside()
+                .into(kannada);
+
+        Picasso.get()
+                .load("https://upload.wikimedia.org/wikipedia/en/5/53/Parasite_%282019_film%29.png")
+                .fit()
+                .centerInside()
+                .into(kodava);
+
+
+
+
+        kannada.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent kannadaintent = new Intent(Categories_activity.this,kannada_activity.class);
+                String cat = "kannada";
+                kannadaintent.putExtra("category",cat);
+                startActivity(kannadaintent);
+            }
+        });
+
+        kodava.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent kodavaintent = new Intent(Categories_activity.this,kannada_activity.class);
+                String cat = "all";
+                kodavaintent.putExtra("category",cat);
+                startActivity(kodavaintent);
+            }
+        });
 
         //when go home button is clicked
         goback.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/args/cineduniya/kannada_activity.java
+++ app/src/main/java/com/args/cineduniya/kannada_activity.java
@@ -1,14 +1,98 @@
 package com.args.cineduniya;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
-public class kannada_activity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class kannada_activity extends AppCompatActivity implements  ImageAdapter.OnItemClickListener {
+    RecyclerView mRecyclerViewdescription;
+    private DatabaseReference mDatabaseRef;
+    private List<Upload> mUploads;
+    private ImageAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kannada_activity);
+
+        mRecyclerViewdescription = findViewById(R.id.recycler_view_kannada);
+
+        int number_column = 3;
+        mRecyclerViewdescription.setHasFixedSize(true);
+        mRecyclerViewdescription.setLayoutManager(new GridLayoutManager(this,number_column));
+
+        mUploads = new ArrayList<>();
+        String category = getIntent().getExtras().getString("category");
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference(""+category);
+
+
+
+
+        mDatabaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+
+                    Upload upload = postSnapshot.getValue(Upload.class);
+                    mUploads.add(upload);
+                }
+
+                mAdapter = new ImageAdapter(kannada_activity.this, mUploads);
+
+                mRecyclerViewdescription.setAdapter(mAdapter);
+
+                mAdapter.setOnItemClickListener(kannada_activity.this);
+
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                Toast.makeText(kannada_activity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
+    }
+
+    @Override
+    public void OnItemClick(String video_url,String movie_name,String poster_url, String banner_url, String Description,Float rating,Float release_year,String trailer)
+    {
+        Intent vid = new Intent(kannada_activity.this,Description_activity.class);
+        String message = video_url;
+        vid.putExtra("trailer",trailer);
+        vid.putExtra("release",release_year);
+        vid.putExtra("rating",rating);
+        vid.putExtra("banner",banner_url);
+        vid.putExtra("poster",poster_url);
+        vid.putExtra("message",message);
+        vid.putExtra("description",Description);
+        String name_of_movie = movie_name;
+        vid.putExtra("movie_name",name_of_movie);
+        startActivity(vid);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
